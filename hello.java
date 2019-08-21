a636fe4fb2f6812a625648852d3b49d04daa2214
cy348215 <940955751@qq.com>
2019-08-22 04:02:02 +0800

    'first'

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
  public Object memberSave(String uname,String upwd,HttpServletRequest request){
         User u=new User();
-        u.setUname(uname);
-        u.setUpwd(upwd);
+      
         User b=userService.findUser(u);
         request.getSession().setAttribute("currentUser",b);
         return "member";
